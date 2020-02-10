66e2a934717d90a209baaf9749a2cc4562d2da30
Patrick Schmidt <prs100791@gmail.com>
2020-02-09 21:15:10 -0600

    Optimizations to VGT-Map

--- VGT-Map.lua
+++ VGT-Map.lua
@@ -4,6 +4,7 @@ local BUFFER_STEP = 10
 local bufferPins = {}
 local players = {}
 local pins = {}
+local isMapVisible = false
 
 local FRAME = "Frame"
 local PLAYER = "player"
@@ -21,13 +22,16 @@ local SCRIPT_ENTER = "OnEnter"
 local SCRIPT_LEAVE = "OnLeave"
 local PIN_TEXTURE = "Interface\\MINIMAP\\ObjectIcons.blp"
 
+local PIN_SIZE = 10
+
 -- PLAYERS INDEXING
 local ACTIVE_INDEX = 1
 local PIN_INDEX = 2
-local MAP_ID_INDEX = 3
-local X_INDEX = 4
-local Y_INDEX = 5
-local HP_INDEX = 6
+local TEXTURE_INDEX = 3
+local MAP_ID_INDEX = 4
+local X_INDEX = 5
+local Y_INDEX = 6
+local HP_INDEX = 7
 
 -- PINS INDEXING
 local NAME_INDEX = 1
@@ -99,52 +103,72 @@ local cleanPins = function()
   end
 end
 
+local validate = function(data)
+  if ((data[ACTIVE_INDEX] == nil or data[ACTIVE_INDEX])
+    and data[PIN_INDEX] ~= nil
+    and data[TEXTURE_INDEX] ~= nil
+    and data[MAP_ID_INDEX] ~= nil
+    and data[X_INDEX] ~= nil
+  and data[Y_INDEX] ~= nil) then
+    return true
+  end
+  return false
+end
+
 local updatePins = function()
   sendMyLocation()
-  cleanPins()
-  if (WorldFrame ~= nil and WorldMapFrame:IsVisible()) then
-    local w, h = WorldFrame:GetWidth(), WorldFrame:GetHeight()
+  if (isMapVisible) then
+    cleanPins()
     for k, v in pairs(players) do
-      if ((v[ACTIVE_INDEX] == nil or v[ACTIVE_INDEX]) and v[PIN_INDEX] ~= nil and v[MAP_ID_INDEX] ~= nil and v[X_INDEX] ~= nil and v[Y_INDEX] ~= nil) then
-        VGT.LIBS.HBDP:AddWorldMapIconWorld(MODULE_NAME, v[PIN_INDEX], tonumber(v[MAP_ID_INDEX]), tonumber(v[X_INDEX]), tonumber(v[Y_INDEX]), 3)
+      if (validate(v)) then
+        local texture = v[TEXTURE_INDEX]
+        if (UnitInParty(k)) then
+          texture:SetTexCoord(0.00, 0.26, 0.26, 0.51) -- Blue
+        else
+          texture:SetTexCoord(0.51, 0.76, 0.00, 0.26) -- Green
+        end
+        VGT.LIBS.HBDP:AddWorldMapIconWorld(MODULE_NAME, v[PIN_INDEX], tonumber(v[MAP_ID_INDEX]), tonumber(v[X_INDEX]), tonumber(v[Y_INDEX]), 3, "PIN_FRAME_LEVEL_WORLD_QUEST")
       end
     end
+    HBD_GetPins().worldmapProvider:RefreshAllData()
   end
 end
 
 local createBufferPins = function()
   for i = 1, BUFFER_STEP do
     local pin = CreateFrame(FRAME, nil, WorldFrame)
-    pin:SetWidth(10)
-    pin:SetHeight(10)
+    pin:SetWidth(PIN_SIZE)
+    pin:SetHeight(PIN_SIZE)
     local texture = pin:CreateTexture(nil, BACKGROUND)
     texture:SetTexture(PIN_TEXTURE)
-    texture:SetTexCoord(0.51, 0.76, 0.00, 0.26)
+    texture:SetTexCoord(0.51, 0.76, 0.00, 0.26) -- Green
     texture:SetAllPoints()
     pin:EnableMouse(true)
     pin:SetScript(SCRIPT_ENTER, onEnterPin)
     pin:SetScript(SCRIPT_LEAVE, onLeavePin)
-    bufferPins[i] = pin
+    bufferPins[i] = {pin, texture}
   end
 end
 
 local findPin = function()
   for i = 1, VGT.Count(bufferPins) do
-    if (bufferPins[i] ~= nil) then
-      local pin = bufferPins[i]
-      bufferPins[i] = nil
-      return pin
+    if (bufferPins[i] ~= nil and bufferPins[i][1] and bufferPins[i][2]) then
+      local pin = bufferPins[i][1]
+      local texture = bufferPins[i][2]
+      bufferPins[i][1] = nil
+      bufferPins[i][2] = nil
+      return pin, texture
     end
   end
   createBufferPins()
 end
 
 local findNextPin = function()
-  local pin
+  local pin, texture
   while (pin == nil) do
-    pin = findPin()
+    pin, texture = findPin()
   end
-  return pin
+  return pin, texture
 end
 
 local handleMapMessageReceivedEvent = function(prefix, message, distribution, sender)
@@ -158,14 +182,23 @@ local handleMapMessageReceivedEvent = function(prefix, message, distribution, se
   end
 
   local playerName, instanceMapId, x, y, hp = strsplit(DELIMITER, message)
-  local pin
+  local pin, texture
   if (players[playerName] == nil) then
-    pin = findNextPin()
+    pin, texture = findNextPin()
   else
     pin = players[playerName][PIN_INDEX]
+    texture = players[playerName][TEXTURE_INDEX]
   end
-  players[playerName] = {true, pin, instanceMapId, x, y, hp}
+  players[playerName] = {true, pin, texture, instanceMapId, x, y, hp}
   pins[pin] = {playerName}
+end
+
+local updateMapVisibility = function()
+  if (WorldMapFrame:IsVisible()) then
+    isMapVisible = true
+  else
+    isMapVisible = false
+  end
 end
 
 -- ############################################################
@@ -176,6 +209,7 @@ function VGT.Map_Initialize()
   if (VGT.OPTIONS.MAP.enabled) then
     createBufferPins()
     VGT.LIBS:RegisterComm(MODULE_NAME, handleMapMessageReceivedEvent)
+    VGT.LIBS:ScheduleRepeatingTimer(updateMapVisibility, 0.1)
     VGT.LIBS:ScheduleRepeatingTimer(updatePins, VGT.OPTIONS.MAP.updateSpeed)
   end
 end
